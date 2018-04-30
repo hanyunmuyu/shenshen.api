f5532feaeff4e242212ff7bef01867e2c00dbea8
hanyun <1355081829@qq.com>
2018-04-30 22:16:06 +0800

    社团活动评论列表

--- app/Http/Controllers/v1/ClubActivityPostController.php
+++ app/Http/Controllers/v1/ClubActivityPostController.php
@@ -11,20 +11,44 @@ namespace App\Http\Controllers\v1;
 
 use App\Http\Controllers\Controller;
 use App\Repositories\v1\ClubActivityPostRepository;
+use App\Repositories\v1\UserCollectionRepository;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ClubActivityPostController extends Controller
 {
     private $clubActivityPostRepository;
-    public function __construct(ClubActivityPostRepository $clubActivityPostRepository)
+    private $userCollectionRepository;
+    public function __construct
+    (
+        ClubActivityPostRepository $clubActivityPostRepository,
+        UserCollectionRepository $userCollectionRepository
+    )
     {
         $this->clubActivityPostRepository = $clubActivityPostRepository;
+        $this->userCollectionRepository = $userCollectionRepository;
     }
 
     public function getClubActivityPostList(Request $request)
     {
-        $clubActivityPostId = $request->get('id');
-        $postList = $this->clubActivityPostRepository->getClubActivityPostListByClubActivityId($clubActivityPostId);
-        return $this->success($postList->toArray());
+        $clubActivityId = $request->get('id');
+        $tag = $request->get('tag');
+        if (!$tag) {
+            return $this->error('tag不可以为空');
+        }
+        if (!$clubActivityId) {
+            return $this->error('id不可以为空');
+        }
+        $postList = $this->clubActivityPostRepository->getClubActivityPostListByClubActivityId($clubActivityId)->toArray();
+        $postList['isFavorite'] = 0;
+        if (Auth::check()) {
+            $user = Auth::user();
+            $uid = $user->id;
+            $collection = $this->userCollectionRepository->getCollection($uid, $tag, $clubActivityId);
+            if ($collection) {
+                $postList['isFavorite'] = 1;
+            }
+        }
+        return $this->success($postList);
     }
 }
