b7f9d0231754adffdd15ea3a87e48c62ba38a1c5
hanyun <1355081829@qq.com>
2018-05-03 15:58:46 +0800

    高校表处理

---
+++ app/Models/CollegeModel.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/5/3 0003
+ * Time: 下午 15:23
+ */
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class CollegeModel extends Model
+{
+    protected $table = 'colleges';
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,17 +13,17 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call('UsersTableSeeder');
-        $this->call(SchoolTableSeeder::class);
-        $this->call(SchoolDepartmentTableSeeder::class);
-        $this->call(SchoolClassTableSeeder::class);
-        $this->call(UserSeeder::class);
-        $this->call(ClubTableSeeder::class);
-        $this->call(ClubUserTableSeeder::class);
-        $this->call(ClubActivityTableSeeder::class);
-        $this->call(ClubActivityPostTableSeeder::class);
-        $this->call(ClubActivityPostPraiseTableSeeder::class);
-        $this->call(HomeRecommendTableSeeder::class);
-        //上面几部已经可以了
-
+//        $this->call(SchoolTableSeeder::class);
+//        $this->call(SchoolDepartmentTableSeeder::class);
+//        $this->call(SchoolClassTableSeeder::class);
+//        $this->call(UserSeeder::class);
+//        $this->call(ClubTableSeeder::class);
+//        $this->call(ClubUserTableSeeder::class);
+//        $this->call(ClubActivityTableSeeder::class);
+//        $this->call(ClubActivityPostTableSeeder::class);
+//        $this->call(ClubActivityPostPraiseTableSeeder::class);
+//        $this->call(HomeRecommendTableSeeder::class);
+//        //上面几部已经可以了
+//        $this->call(CollegeTableSeeder::class);
     }
 }
